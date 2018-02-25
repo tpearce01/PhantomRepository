a6ff189400d4f0128b7f795ce792c1a3684d05ef
Tyler Pearce <pearcetyler@yahoo.com>
2018-02-24 16:05:21 -0800

    Regional, Timed dialogue

--- Phantom/Assets/Scripts/TextObject.cs
+++ Phantom/Assets/Scripts/TextObject.cs
@@ -7,20 +7,46 @@ public class TextObject : MonoBehaviour {
 	float timer;
 	bool activated;
 
+	public float fadeTime = 3.0f;
+
+	void OnEnable(){
+		StartCoroutine (FadeIn());
+	}
+
 	void Update () {
 		timer += Time.deltaTime;
-		if (timer >= 3.0f && !activated) {
+		if (timer >= fadeTime && !activated) {
 			StartCoroutine (Fade());
 			activated = true;
 		}
 	}
 
 	IEnumerator Fade(){
+		Debug.Log ("Fade");
 		Text dialogueText = gameObject.GetComponent<Text> ();
+		dialogueText.color = new Color (dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, 1f);
 		for (int i = 0; i < 50; i++) {
 			dialogueText.color = new Color (dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, dialogueText.color.a - .02f);
 			yield return new WaitForSeconds(0.02f);
 		}
 		Destroy (gameObject);
+	}
+
+	IEnumerator FadeIn(){
+		Text dialogueText = gameObject.GetComponent<Text> ();
+		RectTransform rt = gameObject.GetComponent<RectTransform> ();
+
+		dialogueText.color = new Color (dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, 0f);
+		rt.sizeDelta = new Vector2 (rt.sizeDelta.x,0);
+		for (int i = 0; i < 20; i++) {
+			rt.sizeDelta = new Vector2 (rt.sizeDelta.x,rt.sizeDelta.y + 2);
+			yield return new WaitForSeconds(0.01f);
+		}
+			
+		for (int i = 0; i < 50; i++) {
+			dialogueText.color = new Color (dialogueText.color.r, dialogueText.color.g, dialogueText.color.b, dialogueText.color.a + .05f);
+			yield return new WaitForSeconds(0.02f);
+		}
+
 	}
 }
---
+++ Phantom/Assets/Scripts/TimedDialogue.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimedDialogue : MonoBehaviour {
+
+	[SerializeField] GameObject textObjPrefab;
+	[SerializeField] GameObject dialogueUI;
+	[SerializeField] Dialogue[] conversation;
+	int currentIndex = 0;
+	float timer = 0;
+
+	public void TriggerDialogue(){
+		dialogueUI.SetActive (true);
+	}
+
+	public void Next(){
+		GameObject temp = Instantiate (textObjPrefab, gameObject.transform);
+		temp.GetComponent<Text> ().text = conversation [currentIndex].name;
+		temp.GetComponent<Text> ().color = conversation [currentIndex].GetNameColor ();
+
+		temp = Instantiate (textObjPrefab, gameObject.transform);
+		temp.GetComponent<Text> ().text = conversation [currentIndex].text;
+
+		currentIndex++;
+	}
+
+	[System.Serializable]
+	class Dialogue{
+		public string name;
+		public string text;
+		public float timeToDisplay;
+
+		public Color GetNameColor(){
+			if (name == "test1") {
+				//return Color.blue;
+				return new Color(0,0,1,0);
+			} else if (name == "test2") {
+				//return Color.red;
+				return new Color(1,0,0,0);
+			}
+			return Color.white;
+		}
+	}
+
+	void FixedUpdate(){
+		timer += Time.fixedDeltaTime;
+		if (currentIndex < conversation.Length && timer >= conversation[currentIndex].timeToDisplay) {
+			Debug.Log ("Next");
+			Next ();
+		}
+	}
+
+}
