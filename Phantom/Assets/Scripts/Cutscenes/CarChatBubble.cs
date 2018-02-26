adde73d15e3d2b2c4f7c2948f26b31c2a50bf606
Tyler Pearce <pearcetyler@yahoo.com>
2018-02-26 11:06:35 -0800

    Parallax system, starting cutscene
    - Parallax background system functional
    - Started driving cutscene

---
+++ Phantom/Assets/Scripts/Cutscenes/CarChatBubble.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarChatBubble : MonoBehaviour {
+
+	public float timeBetweenSwap;
+	float timer = 0;
+
+	public GameObject[] chatBubbles;
+	int index = 0;
+
+	public int numChatBubblesToDisplay;
+	int displayed = 0;
+
+	void Update(){
+		timer += Time.deltaTime;
+		if (displayed < numChatBubblesToDisplay && timer >= timeBetweenSwap) {
+			timer = 0;
+			chatBubbles [index].SetActive (false);
+			index = (index + 1) % chatBubbles.Length;
+			chatBubbles [index].SetActive (true);
+			displayed++;
+		} else if (displayed == numChatBubblesToDisplay && timer >= timeBetweenSwap) {
+			chatBubbles [index].SetActive (false);
+			displayed++;
+		}
+	}
+}
