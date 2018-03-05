86b62e78ea78e9def5b247ae2ace05135e8252b8
Tyler Pearce <pearcetyler@yahoo.com>
2018-03-05 11:33:54 -0800

    Cutscene 1

--- Phantom/Assets/Scripts/Cutscenes/CarChatBubble.cs
+++ Phantom/Assets/Scripts/Cutscenes/CarChatBubble.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarChatBubble : MonoBehaviour {
-
+    public float timeToBegin = 0;
 	public float timeBetweenSwap;
 	float timer = 0;
 
@@ -14,16 +14,26 @@ public class CarChatBubble : MonoBehaviour {
 	int displayed = 0;
 
 	void Update(){
-		timer += Time.deltaTime;
-		if (displayed < numChatBubblesToDisplay && timer >= timeBetweenSwap) {
-			timer = 0;
-			chatBubbles [index].SetActive (false);
-			index = (index + 1) % chatBubbles.Length;
-			chatBubbles [index].SetActive (true);
-			displayed++;
-		} else if (displayed == numChatBubblesToDisplay && timer >= timeBetweenSwap) {
-			chatBubbles [index].SetActive (false);
-			displayed++;
-		}
+        if (timeToBegin <= 0) {
+            timer += Time.deltaTime;
+            DisplayBubble();
+        }
+        else {
+            timeToBegin -= Time.deltaTime;
+        }
 	}
+
+    void DisplayBubble() {
+        if (displayed < numChatBubblesToDisplay && timer >= timeBetweenSwap) {
+            timer = 0;
+            chatBubbles[index].SetActive(false);
+            index = (index + 1) % chatBubbles.Length;
+            chatBubbles[index].SetActive(true);
+            displayed++;
+        }
+        else if (displayed == numChatBubblesToDisplay && timer >= timeBetweenSwap) {
+            chatBubbles[index].SetActive(false);
+            displayed++;
+        }
+    }
 }
