3ab99b895eb2716b03f6957ebd16c12ddd315c19
Tyler Pearce <pearcetyler@yahoo.com>
2018-03-05 12:32:03 -0800

    Chat Bubble Improvements, Scene change with fade

--- Phantom/Assets/Scripts/Cutscenes/CarChatBubble.cs
+++ Phantom/Assets/Scripts/Cutscenes/CarChatBubble.cs
@@ -26,14 +26,37 @@ public class CarChatBubble : MonoBehaviour {
     void DisplayBubble() {
         if (displayed < numChatBubblesToDisplay && timer >= timeBetweenSwap) {
             timer = 0;
-            chatBubbles[index].SetActive(false);
+            //chatBubbles[index].SetActive(false);
+            StartCoroutine(FadeOut(index));
             index = (index + 1) % chatBubbles.Length;
-            chatBubbles[index].SetActive(true);
+            //chatBubbles[index].SetActive(true);
+            StartCoroutine(FadeIn(index));
             displayed++;
         }
         else if (displayed == numChatBubblesToDisplay && timer >= timeBetweenSwap) {
-            chatBubbles[index].SetActive(false);
+            //chatBubbles[index].SetActive(false);
+            StartCoroutine(FadeOut(index));
             displayed++;
         }
+    }
+
+    IEnumerator FadeIn(int index) {
+        chatBubbles[index].SetActive(true);
+        SpriteRenderer s = chatBubbles[index].GetComponent<SpriteRenderer>();
+        s.color = new Color(1, 1, 1, 0);
+        for (int i = 0; i < 50; i++) {
+            s.color = new Color(s.color.r, s.color.g, s.color.b, s.color.a + .02f);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    IEnumerator FadeOut(int index) {
+        SpriteRenderer s = chatBubbles[index].GetComponent<SpriteRenderer>();
+        s.color = new Color(1, 1, 1, 1);
+        for (int i = 0; i < 50; i++) {
+            s.color = new Color(s.color.r, s.color.g, s.color.b, s.color.a - .02f);
+            yield return new WaitForSeconds(0.01f);
+        }
+        chatBubbles[index].SetActive(false);
     }
 }
