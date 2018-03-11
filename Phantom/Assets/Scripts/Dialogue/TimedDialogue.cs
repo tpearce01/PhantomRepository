c85b7e83146a68310a5a6eaea965c80c8ad4707d
av7627 <anilverman14@gmail.com>
2018-03-10 17:03:22 -0800

    cutscene diaologue

--- Phantom/Assets/Scripts/Dialogue/TimedDialogue.cs
+++ Phantom/Assets/Scripts/Dialogue/TimedDialogue.cs
@@ -36,20 +36,43 @@ public class TimedDialogue : MonoBehaviour {
     
 		public Color GetNameColor(){
             Debug.Log("Setting name color: " + name);
-            if (name == "Veronica") {
+            if (name == "Veronica")
+            {
                 //return Color.blue;
-                return ConvertColor(83,126,162);
+                return ConvertColor(83, 126, 162);
             }
-            else if (name == "Robbie") {
+            else if (name == "Robbie")
+            {
+                //return Color.green;
+                return ConvertColor(97, 160, 112);
+            }
+            else if (name == "Christopher")
+            {
+                //return Color.purple;
+                return ConvertColor(124, 95, 149);
+            }
+            else if (name == "Tutorial")
+            {
+                //return Color.grey;
+                return ConvertColor(169, 169, 169);
+            }
+            else if (name == "Yvette")
+            {
+                //return Color.pink;
+                return ConvertColor(255,192,203);
+            }
+            else if (name == "Caroline")
+            {
+                //return Color.yellow;
+                return ConvertColor(255,255,0);
+            }
+            else if (name == "Brian")
+            {
                 //return Color.red;
-                return ConvertColor(97,160,112);
+                return ConvertColor(240, 0, 0);
             }
-            else if (name == "Christopher") {
-                //return Color.red;
-                return ConvertColor(124,95,149);
-            }
-        
-            else {
+            else
+            {
 
                 Debug.Log("No valid color");
                 return Color.white;
