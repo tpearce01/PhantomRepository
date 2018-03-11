c6156b58a4a8ee3f9ea6ef85a88f0677c0482e8d
Tyler Pearce <pearcetyler@yahoo.com>
2018-03-10 16:25:28 -0800

    Set up branching save system
    Branching dialogue choices are able to be saved

---
+++ Phantom/Assets/Scripts/TestBranchingDialogue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBranchingDialogue : MonoBehaviour {
+	public GameObject[] panels;
+	int active = 0;
+
+	public void ShowPanel(int index){
+		panels [active].SetActive (false);
+		if (index != -1) {
+			panels [index].SetActive (true);
+		}
+		active = index;
+	}
+
+	public void Choice(int choiceIndex, char decision){
+		PersistentDataManager.SetData (choiceIndex, decision);
+	}
+
+	public void Choice1A(){
+		Choice (0, '1');
+	}
+	public void Choice1B(){
+		Choice (0, '2');
+	}
+
+	public void Choice21A(){
+		Choice (1, '1');
+	}
+	public void Choice21B(){
+		Choice (1, '2');
+	}
+
+	public void Choice22A(){
+		Choice (2, '1');
+	}
+	public void Choice22B(){
+		Choice (2, '1');
+	}
+
+
+	void Start(){
+		PersistentDataManager.LoadData ();
+	}
+}
