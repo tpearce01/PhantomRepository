565940dcabb709e444dcabc090a609cb192086a7
av7627 <anilverman14@gmail.com>
2018-04-19 19:12:16 -0700

    m

--- Phantom/Assets/Scripts/Menu/Map.cs
+++ Phantom/Assets/Scripts/Menu/Map.cs
@@ -9,6 +9,7 @@ public class Map : MonoBehaviour
     public static bool GameIsMap = false;
 
     public GameObject mapMenuUI;
+    public GameObject[] greenDots;
 
     // Update is called once per frame
 
@@ -23,20 +24,35 @@ public class Map : MonoBehaviour
             else
             {
                 Pause();
-                //highlightNode();
             }
         }
     }
     void Resume()
     {
         mapMenuUI.SetActive(false);
+
+       
         Time.timeScale = 1f;
         GameIsMap = false;
+
+        if  (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            greenDots[1].SetActive(false);
+
+        }
     }
 
     void Pause()
     {
         mapMenuUI.SetActive(true);
+
+        if (SceneManager.GetActiveScene().buildIndex == 2) //get scene number to turn on correct dot
+        {
+            greenDots[1].SetActive(true);
+
+        }
+
+
         Time.timeScale = 0f;
         GameIsMap = true;
     }
