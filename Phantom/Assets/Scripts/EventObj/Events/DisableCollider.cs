6d70b29b72aec778decf6f5119cfb08382b65ce5
Naranja-Sagged <alexliuying@gmail.com>
2018-05-13 23:02:25 -0700

    IgnoreCollider script working
    The ignore collider script works now, Is Trigger enabled on second floor

---
+++ Phantom/Assets/Scripts/EventObj/Events/DisableCollider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    DisableCollider
+
+    Purpose:
+        Disable collision between two objects
+     */
+public class DisableCollider : Event {
+   public override IEnumerator TriggerEvent() {
+        GameObject Collider = GameObject.FindGameObjectWithTag("ColliderOpening");
+        GameObject player = GameObject.Find("Player");
+        Physics.IgnoreCollision(Collider.GetComponent<Collider>(), player.GetComponent<Collider>());
+        yield return new WaitForFixedUpdate();
+   }
+
+}
