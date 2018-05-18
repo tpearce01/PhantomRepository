cdbd00775558a662ea23319ba1ce47f545f268ca
Naranja-Sagged <alexliuying@gmail.com>
2018-05-17 20:04:11 -0700

    Incomplete: Added boolean value to GameManager
    Value functions as a check to see if Veronica has been added to party

--- Phantom/Assets/Scripts/EventObj/Events/ShowCharacter.cs
+++ Phantom/Assets/Scripts/EventObj/Events/ShowCharacter.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-    ShowCharacter
+    ShowCharacter (OneTimeEvent)
 
     Purpose:
         Reveal a previously invisible character
+				(Currently only works for Veronica)
      */
 
-public class ShowCharacter : Event {
+public class ShowCharacter : OneTimeEvent {
+		protected override void Start() {
+				base.Start();
+		}
 		public override IEnumerator TriggerEvent() {
+				DeactivateTrigger();
+				//switches isVeronicaAdded(located in GameManager) to true so she can be activated by GameManager for other scenes
+				GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+				gameManager.isVeronicaAdded = true;
+				//------------------------------------------------------------
 				GameObject.Find("Companion2").transform.localScale = new Vector3(1, 1, 1);
 				GameObject.Find("Companion2").GetComponent<Rigidbody>().useGravity = true;
 				GameObject.Find("Companion2").GetComponent<CompanionMovement>().enabled = true;
--- Phantom/Assets/Scripts/GameManager.cs
+++ Phantom/Assets/Scripts/GameManager.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-
+	public bool isVeronicaAdded = true; //checks if Veronica has joined party
 	// Use this for initialization
 	void Start () {
+				Debug.Log("GAMEMANAGER START CALLED");
+				if (isVeronicaAdded == true) {
+						Debug.Log("ISVERONICAADDED IS TRUE!!!!!!!!!");
+						GameObject.Find("Companion2").transform.localScale = new Vector3(1, 1, 1);
+						GameObject.Find("Companion2").GetComponent<Rigidbody>().useGravity = true;
+						GameObject.Find("Companion2").GetComponent<CompanionMovement>().enabled = true;
+				}
+				if (isVeronicaAdded == false) {
+						Debug.Log("ISVERONICAADDED IS FALSE!!!!!!!!");
+				}
+
         // Handle Configuration settings
         if (Config.ClearSaveDataOnStart) {
             SaveData.DeleteData();
