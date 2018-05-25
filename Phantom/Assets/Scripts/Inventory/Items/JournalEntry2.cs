20fbb032ce939219c6b198d98a380a008dad7114
jdiaz2427 <jessediaz2427@yahoo.com>
2018-05-25 00:03:57 -0700

    tat

---
+++ Phantom/Assets/Scripts/Inventory/Items/JournalEntry2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalEntry2 : Item
+{
+    void Awake()
+    {
+        itemName = "RustedKey";
+        Texture2D tex = Resources.Load<Texture2D>("RustedKey");
+        image = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0, 0));
+    }
+
+    public override IEnumerator TriggerEvent()
+    {
+        // Event Code Goes Here
+        yield break;
+    }
+}
