24781568c3d8d03ec0d1370c90734014f2812c4f
Naranja-Sagged <alexliuying@gmail.com>
2018-05-24 19:47:04 -0700

    Tablet and Journal Entry #5 added to inside of haunted house
    Event to get them added. Dialogue also added for Christopher when Journal #5 is added to inventory

--- Phantom/Assets/Scripts/Inventory/Items/Tablet.cs
+++ Phantom/Assets/Scripts/Inventory/Items/Tablet.cs
@@ -3,9 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RitualRoomPodium : Item {
+public class Tablet : Item {
 		void Awake() {
-			itemName = "Ritual Room Podium";
+			itemName = "Tablet";
 			Texture2D tex = Resources.Load<Texture2D>("RustedKey");
 			image = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0, 0));
 		}
