2cc72ff523bdcb162e57c6bb406799cfe7614634
jdiaz2427 <jessediaz2427@yahoo.com>
2018-05-27 19:57:53 -0700

    for

--- Phantom/Assets/Scripts/Inventory/Items/CoasterControlRoomKey.cs
+++ Phantom/Assets/Scripts/Inventory/Items/CoasterControlRoomKey.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JournalEntry2 : Item
+public class CoasterControlRoomKey : Item
 {
     void Awake()
     {
-        itemName = "JournalEntry2";
-        Texture2D tex = Resources.Load<Texture2D>("JournalEntry2");
+        itemName = "CoasterControlRoomKey";
+        Texture2D tex = Resources.Load<Texture2D>("CoasterControlRoomKey");
         image = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0, 0));
     }
 
