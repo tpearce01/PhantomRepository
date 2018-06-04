4ee0d399397894d2a45bbffab1be852c68b50eca
Naranja-Sagged <alexliuying@gmail.com>
2018-06-04 13:37:54 -0700

    changesceneevent fix
    merge

--- Phantom/Assets/Scripts/EventObj/Events/ChangeSceneEventHHI.cs
+++ Phantom/Assets/Scripts/EventObj/Events/ChangeSceneEventHHI.cs
@@ -1,15 +1,14 @@
-/*﻿using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+/*
     Event to change scenes. Specift the scene to load from the dropdown in the Unity inspector.
     blackOverlayPrefab should not be changed - It needs to be the overlay prefab to fade out properly
-
-public class ChangeSceneEvent : Event {
-    public Scenes sceneToLoad;              // Scene to load
+     */
+public class ChangeSceneEventHHI : Event {
     public float delay;                     // Delay before the fading starts
     public GameObject blackOverlayPrefab;   // Black screen overlay
     float fadeTime = 3;                     // Fade duration
@@ -21,9 +20,8 @@ public class ChangeSceneEvent : Event {
         overlay.FadeIn(fadeTime);
         yield return new WaitForSeconds(fadeTime);
 
-        SceneManager.LoadScene();
+        SceneManager.LoadScene("HauntedHouseInside");
         yield break;
     }
 
 }
-*/
