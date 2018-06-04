858dfbd00f6121eba9b7d27da69d76afad9b8df0
av7627 <anilverman14@gmail.com>
2018-06-04 00:25:45 -0700

    insideemplloung

---
+++ Phantom/Assets/Scripts/EventObj/Events/TriggerOnItems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TriggerOnItems : MonoBehaviour
+{
+    public GameObject doll;
+    public GameObject Jounral1;
+    public GameObject dinerKey;
+
+    public GameObject Player_caroline;
+
+    public GameObject dialogue;
+    //public GameObject AppearingTimer;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if all these items are collected, set caroline active
+        if((doll.GetComponent("Fade") as Fade) != null & (Jounral1.GetComponent("Fade") as Fade) != null & (dinerKey.GetComponent("Fade") as Fade) != null)
+        {
+            Player_caroline.SetActive(true);
+        }
+    }
+    
+}
