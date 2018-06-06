1d1e78583f699c185f4c60c7259d0ebb9cb18474
Tyler Pearce <tpearce01@users.noreply.github.com>
2018-06-05 22:30:54 -0700

    Resetting config to not autosave

--- Phantom/Assets/Scripts/Config.cs
+++ Phantom/Assets/Scripts/Config.cs
@@ -3,6 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class Config {
-    public const bool AutoSave = true;
-    public const bool ClearSaveDataOnStart = false;
+    public const bool AutoSave = false;
+    public const bool ClearSaveDataOnStart = true;
 }
