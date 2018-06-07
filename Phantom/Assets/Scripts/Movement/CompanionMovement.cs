8403127856423f8f9b43bd273ec7eb7de4f04fa6
Kiara-Producer <32949853+Kiara-Producer@users.noreply.github.com>
2018-06-06 18:12:40 -0700

    Stuff

--- Phantom/Assets/Scripts/Movement/CompanionMovement.cs
+++ Phantom/Assets/Scripts/Movement/CompanionMovement.cs
@@ -61,6 +61,7 @@ public class CompanionMovement : MonoBehaviour {
             }
             else
             {
+                
                 animator.SetBool("Walking", true);
                 if (agent.velocity.x > 0)
                 {
