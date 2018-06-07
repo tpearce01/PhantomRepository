92373e2a42ab67bfcd47c31563110950ad282cf5
Naranja-Sagged <alexliuying@gmail.com>
2018-06-06 18:11:28 -0700

    Fixed Stuff

--- Phantom/Assets/Scripts/Inventory/Items/JournalEntry4.cs
+++ Phantom/Assets/Scripts/Inventory/Items/JournalEntry4.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class JournalEntry4 : Item {
 		void Awake() {
 			itemName = "Journal Entry 4";
-			Texture2D tex = Resources.Load<Texture2D>("RustedKey");
+			Texture2D tex = Resources.Load<Texture2D>("JournalEntry4");
 			image = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0, 0));
 		}
 
